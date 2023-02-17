ef99e50ca4c6a127aca97b85686b7657db8f3034
Ajith <ajithclassy@outlook.com>
2023-02-17 13:11:37 +0530

    another dpname has added

--- aswin_sample/connection.php
+++ aswin_sample/connection.php
@@ -3,14 +3,15 @@
 
 $servername = "localhost";
 $username = "root";
-
 $password = "qwerty";
-
 $dbname = "login";
+$dbname1= "";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+
 
 ?>
