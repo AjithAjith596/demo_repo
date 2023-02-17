c5440d11eaaa9b4b4f93d30783cd6a54dbc8b9de
Ajith <ajithclassy@outlook.com>
2023-02-17 12:17:53 +0530

    dbname has been removed in connection file

--- aswin_sample/connection.php
+++ aswin_sample/connection.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "login";
+$dbname = "";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
