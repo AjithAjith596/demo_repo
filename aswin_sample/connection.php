0bbec54e8ac5edfffc1011e8709271e592c87e9f
Ajith <ajithclassy@outlook.com>
2023-02-17 12:40:43 +0530

    new password has added in connection

--- aswin_sample/connection.php
+++ aswin_sample/connection.php
@@ -3,7 +3,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "12356";
 $dbname = "login";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
